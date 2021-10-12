6dd901a87dd270ff5b10db30df93b5862ad2a607
yara mustafa <mustafa.yara@elev.ga.ntig.se>
2021-10-12 20:01:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace why_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inmatat = string.Empty;
+
+            while (inmatat != "hej")
+            {
+                 Console.Write("mata in något: ");
+                 inmatat = Console.ReadLine();
+                 
+                 Console.WriteLine("Du skrev:" + inmatat);
+            }
+             Console.WriteLine("Nu är programet slut!");
+
+        }
+    }
+}
